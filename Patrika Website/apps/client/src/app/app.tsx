41faf3d19d921e2d44e40f3b2fb0da5a8795e91f
parvg555 <parvg555@gmail.com>
2022-01-21 22:00:56 +0530

    initial setup

---
+++ Patrika Website/apps/client/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+
+import Index from './components/Index';
+
+export function App() {
+  return (
+    <>
+      <Index />
+    </>
+  );
+}
+
+export default App;
---
+++ Patrika Website/apps/client/src/app/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Web3 from 'web3';
+
+import './css/Index.css'
+
+function Index() {
+  return <div className='index'>
+      START CODING HERE
+  </div>;
+}
+
+export default Index;
