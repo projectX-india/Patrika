630fa8b038efe5414076cb9b14367692da62b6dd
parvg555 <parvg555@gmail.com>
2022-02-26 19:13:20 +0530

    design update

--- Patrika Website/apps/client/src/app/components/CardProfile.js
+++ Patrika Website/apps/client/src/app/components/CardProfile.js
@@ -12,17 +12,26 @@ function CardProfile(){
 
     return(
         <div className='CardProfile'>
-            <img src="https://i.imgur.com/2DhmtJ4.jpg" class="CardProfile-cover" alt="" />
+            <img src="https://i.imgur.com/2DhmtJ4.jpg" className="CardProfile-cover" alt="" />
             <div className='CardProfile-overlay'>
                 <div className='CardProfile-header'>
-                    <svg class="CardProfile-arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                    <img class="CardProfile-thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-                    <div class="CardProfile-thumb-text">
-                        <h3 class="CardProfile-thumb-title">kim Cattrall</h3>
-                        <span class="CardProfile-thumb-status">3 hours ago</span>
-                    </div>  
+                    <svg className="CardProfile-arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                    <div className='CardProfile-thumb-container'>
+                        <img className="CardProfile-thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+                        <div className="CardProfile-thumb-text">
+                            <h3 className="CardProfile-thumb-title">
+                                kim Cattrall
+                            </h3>
+                            <span
+                            className="CardProfile-thumb-status">
+                                3 hours ago
+                            </span>
+                        </div>
+                    </div>
                 </div>
-
+                <p className='CardProfile-bio'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+                </p>
             </div>
         </div>
     );
