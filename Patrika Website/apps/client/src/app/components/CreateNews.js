92348060e6b634a00e184cf6f7c22bfa726e242f
parvg555 <parvg555@gmail.com>
2022-02-26 23:46:21 +0530

    form input stucture

--- Patrika Website/apps/client/src/app/components/CreateNews.js
+++ Patrika Website/apps/client/src/app/components/CreateNews.js
@@ -7,6 +7,8 @@ import './css/CreateNews.css';
 /* Component import */
 
 /* Asset imports */
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 
 function CreateNews(){
 
@@ -16,6 +18,7 @@ function CreateNews(){
     };
     
     const [inputArray, setInputArray] = useState([textInput]);
+    const [headline, setHeadline] = useState('');
 
     const addParagraphSection = () => {
         setInputArray( inputArray => {
@@ -53,27 +56,66 @@ function CreateNews(){
 
     return(
         <div className='CreateNews'>
-            <button onClick={() => addParagraphSection()}>
+
+            
+            {/* <button onClick={() => addParagraphSection()}>
                 ADD PARAGRAPH
             </button><br/>
             <button
                 onClick={() => addFileSection()}
             >
                 ADD FILE
-            </button><br/>
-            <button>ADD IMAGE</button>
+            </button><br/> */}
+            <input 
+                type="text" 
+                className='text-input'
+                value={headline}
+                onChange={(e) => setHeadline(e.target.value)}
+                placeholder='Headline...'
+            />
             {
                 inputArray.map((item,i) => {
-                    return(
-                        <input
-                            onChange={handleChange}
-                            value={item.value}
-                            id={i}
-                            type={item.type}
-                        />
-                    )
+                    if(item.type == 'text'){
+                        return(
+                            <textarea
+                                className='text-input'
+                                onChange={handleChange}
+                                value={item.value}
+                                id={i}
+                                type={item.type}
+                                placeholder='Type text here...'
+                                rows={"10"}
+                            />
+                        )
+                    }else{
+                        return(
+                            <div className='image-holder'>
+                                <input
+                                    className='img-input'
+                                    onChange={handleChange}
+                                    value={item.value}
+                                    id={i}
+                                    type={item.type}
+                                />
+                            </div>
+                            
+                        )
+                    }
                 })
             }
+            <div className='Create-options'>
+                <AddBoxIcon
+                    onClick={()=>addParagraphSection()}
+                />
+                <AddPhotoAlternateIcon
+                    onClick={()=>addFileSection()}
+                />
+            </div>
+            <div className="submit-section">
+                <button>
+                    POST ARTICLE
+                </button>
+            </div>
         </div>
     );
 }
